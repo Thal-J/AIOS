c8203409ef32ba8f90a82e6b54f65271e3c76d24
F-u-l-l-e-r-t-o-n <34149782+F-u-l-l-e-r-t-o-n@users.noreply.github.com>
2018-11-17 14:29:30 +1300

    Update bot.py
    Fixed errors in code

--- bot.py
+++ bot.py
@@ -8,30 +8,31 @@ tokenFile = open("token.txt", "r")
 token=str(tokenFile.read())
 
 @client.event
-async def exampleembedoutput():
+async def on_message(messagename):
     examplembed = discord.Embed(
-        title = 'Retrieving information'
-        description = 'Dreaming—torchbugs—overturned jar'
-        colour = discord.Colour.yellow()
+        title = 'Retrieving information',
+        description = 'Dreaming—torchbugs—overturned jar',
+        colour = discord.Colour.red()
     )
-    examplembed.set_footer(text='AIOS functionality delivered')
+    examplembed.set_footer(text='AIOS functionality preparing')
     examplembed.set_image(url='https://media.discordapp.net/attachments/494443546989035520/509201412672978944/unknown.png?width=888&height=500')
     examplembed.set_thumbnail(url='https://cdn.discordapp.com/attachments/369940161796112394/512618874689290261/k86qvkS.png')
     examplembed.set_author(name='AIOS', icon_url='https://cdn.discordapp.com/attachments/369940161796112394/512618874689290261/k86qvkS.png')
     
     NewEmbed = discord.Embed(
-        title = 'Information Retrieved'
-        description = 'Sample Information'
+        title = 'Information Retrieved',
+        description = 'Sample Information',
         colour = discord.Colour.green()
     )
     NewEmbed.set_footer(text='AIOS functionality delivered')
-    NewEmbed.set_image(url='https://media.discordapp.net/attachments/494443546989035520/509201412672978944/unknown.png?width=888&height=500')
+    NewEmbed.set_image(url='https://cdn.discordapp.com/attachments/494443546989035520/512170758848380928/unknown.png')
     NewEmbed.set_thumbnail(url='https://cdn.discordapp.com/attachments/369940161796112394/512618874689290261/k86qvkS.png')
     NewEmbed.set_author(name='AIOS', icon_url='https://cdn.discordapp.com/attachments/369940161796112394/512618874689290261/k86qvkS.png')
-    
-    await client.send_message(message.channel, embed=examplembed)
-    time.sleep(120)
-    edit_message(message, new_content=None, *, embed=NewEmbed)
+
+    if messagename.content==('Output'):
+        oldmessage = await client.send_message(messagename.channel, ('speech'), embed=examplembed)
+        time.sleep(15)
+        await client.edit_message(oldmessage, new_content=None, embed=NewEmbed)
 
 @client.event
 async def on_ready():
